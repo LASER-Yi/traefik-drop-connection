60d14ce4cbb8b7cbb01c6634324e625858865f6b
LASER-Yi <liangyi0007@gmail.com>
2022-12-03 19:58:18 +0800

    Init basic logic

--- drop_connection.go
+++ drop_connection.go
@@ -1,65 +1,123 @@
-package drop_connection
+package traefik_drop_connection
 
 import (
 	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"text/template"
+	"strconv"
+	"strings"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	Headers map[string]string `json:"headers,omitempty"`
+	StatusCode string `json:"status_code,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		Headers: make(map[string]string),
+		StatusCode: "",
 	}
 }
 
-// Demo a Demo plugin.
-type Demo struct {
-	next     http.Handler
-	headers  map[string]string
-	name     string
-	template *template.Template
+type dropConnection struct {
+	next              http.Handler
+	name              string
+	status_code_start int
+	status_code_end   int
 }
 
-// New created a new Demo plugin.
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	if len(config.Headers) == 0 {
-		return nil, fmt.Errorf("headers cannot be empty")
+
+	start_code := 0
+	end_code := 0
+	if len(config.StatusCode) != 0 {
+		codes := strings.Split(config.StatusCode, "-")
+
+		if len(codes) != 2 {
+			return nil, fmt.Errorf("error compiling status code mapping")
+		}
+
+		start, err := strconv.Atoi(codes[0])
+
+		if err != nil {
+			return nil, fmt.Errorf("error converting first status code to integer, %s", err)
+		}
+
+		end, err := strconv.Atoi(codes[1])
+
+		if err != nil {
+			return nil, fmt.Errorf("error converting second status code to integer, %s", err)
+		}
+
+		start_code = start
+		end_code = end
 	}
 
-	return &Demo{
-		headers:  config.Headers,
-		next:     next,
-		name:     name,
-		template: template.New("demo").Delims("[[", "]]"),
+	return &dropConnection{
+		next:              next,
+		name:              name,
+		status_code_start: start_code,
+		status_code_end:   end_code,
 	}, nil
 }
 
-func (a *Demo) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	for key, value := range a.headers {
-		tmpl, err := a.template.Parse(value)
+func (a *dropConnection) ResetConn(w http.ResponseWriter, req *http.Request) {
+	if wr, ok := w.(http.Hijacker); ok {
+		conn, _, err := wr.Hijack()
 		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			log.Println(w, err)
 			return
 		}
+		conn.Close()
+	} else {
+		log.Println("Cannot reset connection, the hijacker is not existed")
+	}
+}
 
-		writer := &bytes.Buffer{}
+func (a *dropConnection) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	// TODO: Check condition
+	if a.status_code_start != 0 && a.status_code_end != 0 {
+		// Let's send a request to the next chain and wait for the feedback
+		wrappedWriter := &responseWriter{}
 
-		err = tmpl.Execute(writer, req)
-		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
+		log.Printf("read from upstream and waiting for the status code between %d and %d", a.status_code_start, a.status_code_end)
+
+		a.next.ServeHTTP(wrappedWriter, req)
+
+		statusCode := wrappedWriter.statusCode
+		if statusCode == 0 {
+			statusCode = 200
 		}
 
-		req.Header.Set(key, writer.String())
+		bodyBytes := wrappedWriter.buffer.Bytes()
+
+		log.Printf("received status code %d", statusCode)
+
+		if statusCode < a.status_code_start || a.status_code_end < statusCode {
+			rw.Write(bodyBytes)
+			return
+		}
 	}
 
-	a.next.ServeHTTP(rw, req)
+	a.ResetConn(rw, req)
+}
+
+type responseWriter struct {
+	buffer     bytes.Buffer
+	statusCode int
+
+	http.ResponseWriter
+}
+
+func (r *responseWriter) WriteHeader(statusCode int) {
+	r.statusCode = statusCode
+
+	r.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (r *responseWriter) Write(p []byte) (int, error) {
+	return r.buffer.Write(p)
 }
--- drop_connection_test.go
+++ drop_connection_test.go
@@ -1,26 +1,29 @@
-package drop_connection_test
+package traefik_drop_connection_test
 
 import (
+	"bytes"
 	"context"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	drop_connection "github.com/LASER-Yi/traefik-drop-connection"
+	traefik_drop_connection "github.com/LASER-Yi/traefik-drop-connection"
 )
 
-func TestDemo(t *testing.T) {
-	cfg := drop_connection.CreateConfig()
-	cfg.Headers["X-Host"] = "[[.Host]]"
-	cfg.Headers["X-Method"] = "[[.Method]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-URL"] = "[[.URL]]"
-	cfg.Headers["X-Demo"] = "test"
+var defaultCtx = []byte("It works!")
+
+func DefaultContextHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Write(defaultCtx)
+}
+
+func TestDropConnection(t *testing.T) {
+	cfg := traefik_drop_connection.CreateConfig()
 
 	ctx := context.Background()
-	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+	next := http.HandlerFunc(DefaultContextHandler)
 
-	handler, err := drop_connection.New(ctx, next, cfg, "demo-plugin")
+	handler, err := traefik_drop_connection.New(ctx, next, cfg, "drop-connection-plugin")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -34,16 +37,77 @@ func TestDemo(t *testing.T) {
 
 	handler.ServeHTTP(recorder, req)
 
-	assertHeader(t, req, "X-Host", "localhost")
-	assertHeader(t, req, "X-URL", "http://localhost")
-	assertHeader(t, req, "X-Method", "GET")
-	assertHeader(t, req, "X-Demo", "test")
+	result := recorder.Result()
+
+	assertBody(t, result.Body, make([]byte, 0))
 }
 
-func assertHeader(t *testing.T, req *http.Request, key, expected string) {
+func TestDropConnectionOutsideRange(t *testing.T) {
+	cfg := traefik_drop_connection.CreateConfig()
+	cfg.StatusCode = "300-599"
+
+	ctx := context.Background()
+	next := http.HandlerFunc(DefaultContextHandler)
+
+	handler, err := traefik_drop_connection.New(ctx, next, cfg, "drop-connection-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	result := recorder.Result()
+
+	if result.StatusCode != 200 {
+		t.Error("The context handler should return 200")
+	}
+
+	assertBody(t, result.Body, defaultCtx)
+}
+
+func TestDropConnectionInsideRange(t *testing.T) {
+	cfg := traefik_drop_connection.CreateConfig()
+	cfg.StatusCode = "100-599"
+
+	ctx := context.Background()
+	next := http.HandlerFunc(DefaultContextHandler)
+
+	handler, err := traefik_drop_connection.New(ctx, next, cfg, "drop-connection-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	result := recorder.Result()
+
+	if result.StatusCode != 200 {
+		t.Error("The context handler should return 200")
+	}
+
+	assertBody(t, result.Body, make([]byte, 0))
+}
+
+func assertBody(t *testing.T, body io.Reader, expected []byte) {
 	t.Helper()
 
-	if req.Header.Get(key) != expected {
-		t.Errorf("invalid header value: %s", req.Header.Get(key))
+	if resp, err := io.ReadAll(body); err == nil {
+		if bytes.Compare(resp, expected) != 0 {
+			t.Errorf("invalid body content \"%s\", should be \"%s\"", string(resp), string(expected))
+		}
 	}
 }
